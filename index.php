59cce3ed944476e34fa5131d829b12cee843a518
yusev <32067422+yusev@users.noreply.github.com>
2017-10-11 00:45:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,134 @@
-	<html>
-	<head>
-	<title>Inicio de sesión</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-	</head>
-	<body>
-	<form action="control.php" method="post" id="form"><br>
-	Usuario: <input type="text" name="usuario" id="usuario" /><br>
-	Clave: <input type="password" name="clave" id="clave" /><br>
-	<input type="submit" value="Entrar">
-	</form>
-	</body>
-	</html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><center> <h2>REGISTRO DE USUARIOS</title></h2> </center>
+
+</head>
+
+<body>
+<style type="text/css">
+<!--
+
+a:link {  color: #FFFFFF; text-decoration: none}
+
+a:visited {  text-decoration: none; color: #FFCC66}
+
+a:hover {  color: #99CCFF; text-decoration: underline}
+body {  font-family: "Arial", "Helvetica", "sans-serif"}
+
+-->
+</style>
+</head>
+
+<body bgcolor="#FFFFFF" link="#FFFFFF">
+<table background="" width="1000" border="0" cellspacing="0" cellpadding="0" height="75" align="center" >
+  
+    <td colspan="2"><pre><h1>REGISTRO DE USUARIOS</h1>                                                                  </pre>
+     
+    </td>
+  </tr>
+</table>
+
+<table width="1000" border="0" cellspacing="0" cellpadding="0" height="20" align="center">
+  <tr bgcolor="#000000"> 
+    <td>
+      <div align="center"><a href="#"></a> <a href="#"></a> <a href="#"></a> 
+        <font color="#FFFFFF"><a href="#"></a></font></div>
+    </td>
+  </tr>
+</table>
+<table width="1000" border="0" cellspacing="0" cellpadding="0" height="13" background="image/top1.gif" align="center">
+  <tr>
+    <td><img src="image/pixel.gif" width="1" height="1"></td>
+  </tr>
+</table>
+<br>
+<table width="1000" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr> 
+   
+   
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<form action="validaregistro.php" method="post" >
+    
+   
+  <div align="center">
+   
+<tr>
+        <td><h4>*LLENAR CADA CAMPO </h4>
+          <label for="textfield"></label>
+          <table width="257" height="76" border="0">
+          <tr>
+            <td width="96" height="33"><div align="right"><strong> <td bgcolor="red">
+ Login:</strong></div></td>
+     
+       <td width="145"><label for="textfield2"></label>
+            <input type="input_field" name="usuario" id="usuario"></td>
+          </tr>
+          <tr>
+            <td><div align="right"><strong><td bgcolor="blue">
+Password:</strong></div></td>
+            <td><label for="textfield3"></label>
+            <input class="input_field"  type="password" name="contra"  id="contra" placeholder="MAXIMO 6 "></td>
+          </tr>
+          <tr>
+            <td><div align="right"><strong><td bgcolor="blue">
+E-mail:</strong></div></td>
+            <td><input type="text" name="email" id="email" /></td>
+          </tr>
+          <tr>
+            <td><div align="right"><strong><td bgcolor="blue">
+Nombre:</strong></div></td>
+            <td><label for="textfield4"></label>
+            <input type="text" name="nombre" id="nombre" /></td>
+          </tr>
+          <tr>
+            <td><div align="right"><strong><td bgcolor="blue">
+Apellidos:</strong></div></td>
+            <td><label for="textfield5"></label>
+            <input type="text" name="apellido" id="apellido" /></td>
+          </tr>
+          <tr>
+            <td><div align="right"><strong><td bgcolor="blue">
+Direccion:</strong></div></td>
+            <td><label for="textfield6"></label>
+            <input type="text" name="direccion" id="direccion" /></td>
+          </tr>
+          <tr>
+            <td><div align="right"><strong><td bgcolor="blue">Telefono:</strong></div></td>
+            <td><label for="textfield7"></label>
+            <input type="text" name="telefono" id="telefono" /></td>
+          </tr>
+          <tr>
+            <td><div align="right"><strong><td bgcolor="blue">
+CP:</strong></div></td>
+            <td><label for="textfield8"></label>
+            <input type="text" name="cp" id="cp" /></td>
+          </tr>
+ <center>  <img src="Imagenes/SEMAR.jpg"WIDTH=300 HEIGHT=200></center>         </table>
+        <p>
+          <input type="submit" name="button" id="button" value="REGISTRARME">
+      </p></td>
+      </tr>
+  </div>
+</form>
+<p>&nbsp;</p>
+<p><br>
+</p>
+<table width="473" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr> 
+    <td rowspan="2" width="65"> 
+      <div align="left"><font size="2" face="Arial, Helvetica, sans-serif"><font color="#3333FF"><img src="image/logo.gif" width="50" height="48"></font> 
+        </font></div>
+      <font size="2" face="Arial, Helvetica, sans-serif"> <font color="#3333FF"> 
+      </font> </font></td>
+    <td width="408"><font size="2" face="Arial, Helvetica, sans-serif"><font color="#006699">SEMARNAT@hotmail.com</font></font></a>      </font></font></td>
+  </tr>
+</table>
+<a></a>
+
+</body>
+</html>
